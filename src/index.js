77054dfd55bdc3766d758b9b3d1b50cfbd71c526
jefiedler <jonas.fiedler@magenta.de>
2020-08-05 12:20:36 +0200

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { createTitle } from "./components/title.js";
+import { createToggleButton } from "./components/toggleButton.js";
+
+const title = createTitle();
+
+const toggleButton = createToggleButton();
+
+document.body.append(title);
+document.body.append(toggleButton);
