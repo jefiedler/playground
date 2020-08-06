6b318c4482129f38775dc7f985dedf1f51a6cc13
jefiedler <jonas.fiedler@magenta.de>
2020-08-06 11:10:44 +0200

    Add script liveServer

--- src/components/title.js
+++ src/components/title.js
@@ -2,7 +2,7 @@ import { createElement } from "../utils/elements.js";
 
 export function createTitle(){
     const title = createElement("h1", { 
-        innerHTML: "Playground"
+        innerHTML: "My Playground!"
     });
     return title;
 };
